4a739d7b4232f738d6f8ffb1d1574cef61d3a189
Ryan Wittman <OfficiallyRy@outlook.com>
2023-03-21 14:59:09 -0600

    Re-adding all the stuff from lecture

--- Startup.cs
+++ Startup.cs
@@ -33,6 +33,8 @@ public class Startup
 
     services.AddScoped<AccountsRepository>();
     services.AddScoped<AccountService>();
+    services.AddScoped<CarsRepository>();
+    services.AddScoped<CarsService>();
   }
 
   private void ConfigureCors(IServiceCollection services)
