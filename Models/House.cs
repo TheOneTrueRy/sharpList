749ff36a97c7dd8128d4ce5845e2b32deed2528f
Ryan Wittman <OfficiallyRy@outlook.com>
2023-03-21 15:11:27 -0600

    Was missing Id and Year

--- Models/House.cs
+++ Models/House.cs
@@ -7,9 +7,11 @@ namespace sharpList.Models
 {
   public class House
   {
+    public int Id { get; set; }
     public string Address { get; set; }
     public int Bedrooms { get; set; }
     public int Bathrooms { get; set; }
+    public int Year { get; set; }
     public int Floors { get; set; }
     public int SqFt { get; set; }
     public string Description { get; set; }
